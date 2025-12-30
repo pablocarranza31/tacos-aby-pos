5c7af530470cce827200ab5b08d82740de6b1d5d
pablocarranza31 <158582000+pablocarranza31@users.noreply.github.com>
2025-12-30 14:15:38 -0600

    Agregue configuración del cors

--- Backend_tacosABY/index.js
+++ Backend_tacosABY/index.js
@@ -5,12 +5,25 @@ const pool = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',              // Vite local
+    'http://localhost:3000',              // por si acaso
+    'https://pablocarranza31.github.io'   // GitHub Pages
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 const authRouter = require('./auth');
 const { crearUsuario } = require('./usuarios.controller');
 const verificarToken = require('./middleware/auth');
 const soloAdmin = require('./middleware/soloAdmin');
+
+// Rutas
+app.get('/', (req, res) => {
+  res.send('API funcionando correctamente 🚀');
+});
 
 
 app.post('/usuarios',verificarToken,soloAdmin, crearUsuario);
