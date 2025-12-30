29a4814bba9e27d22ca39f5a1d6cccb725420960
pablocarranza31 <158582000+pablocarranza31@users.noreply.github.com>
2025-12-30 13:52:20 -0600

    pruebas para subir proyecto a git pages

--- Front_tacosABY/src/main.jsx
+++ Front_tacosABY/src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // Proveedor de enrutamiento basado en el historial del navegador
+import { HashRouter } from 'react-router-dom' // Proveedor de enrutamiento basado en el hash de la URL
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
