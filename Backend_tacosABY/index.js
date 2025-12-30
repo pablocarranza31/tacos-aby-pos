81e4897b5937987bd64f2256711e1c4f8de0a70a
pablocarranza31 <158582000+pablocarranza31@users.noreply.github.com>
2025-12-30 14:07:22 -0600

    update para subir a render el backend

--- Backend_tacosABY/index.js
+++ Backend_tacosABY/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const pool = require('./db');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 const authRouter = require('./auth');
@@ -11,10 +12,11 @@ const { crearUsuario } = require('./usuarios.controller');
 const verificarToken = require('./middleware/auth');
 const soloAdmin = require('./middleware/soloAdmin');
 
+
 app.post('/usuarios',verificarToken,soloAdmin, crearUsuario);
 
 app.use('/auth', authRouter);
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
